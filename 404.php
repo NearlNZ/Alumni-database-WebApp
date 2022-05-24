515cf184990f0689dee93cae03fbfa03086e9a03
Nearl <mynetdemo@gmail.com>
2022-05-24 22:05:46 +0700

    V0.1 (blank web)

---
+++ 404.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>Blank Page Visitor</title>
+        
+        <!-- Include Fonts / Icons -->
+        <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Fredoka One" rel="stylesheet">
+        <script src="fontawesome-6.1.1/js/all.min.js"></script>
+        <!-- Include CSS / Js -->
+        <link href="css/styles.css" rel="stylesheet" />
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/scripts.js"></script>
+        <!-- Custom Style -->
+        <STYLE type="text/css">
+            body {
+                font-family: "Kanit";
+            }
+            #mid {
+                vertical-align: middle !important;
+            }
+        </STYLE>
+    </head>
+    <body style="background-color: #EAEAEA;">
+        <!-- Content -->
+        <main>
+            <div class="text-center">
+                <br><br><br><br><br><br><br><br><br>
+                <h1 id="mid" class="display-1">404</h1>
+                <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
+                <p class="lead">
+                    The page you’re looking for doesn’t exist.
+                </p>
+                <a class="fs-5 nav-link" role="button" onclick="history.back()"><i class="fa-solid fa-arrow-left me-2"></i>Go back to website</a>
+            </div>  
+        </main>
+        <!-- End Content -->
+    </body>
+</html>
